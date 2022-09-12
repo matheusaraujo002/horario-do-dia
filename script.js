9f51ce9d4e6e525856703ba2c341f2afa8d29449
Matheus <matheusdearaujo1209@gmail.com>
2022-09-12 19:56:42 -0300

    Horário do Dia

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
     function carregar(){
     var msg = window.document.getElementById('texto')
     var msg2 = window.document.getElementById('texto2')
+    var msg3 = window.document.getElementById('texto3')
     var data = new Date()
     var fusoHorario = Intl.DateTimeFormat().resolvedOptions().timeZone;
     var hora = data.getHours()
     var minuto = data.getMinutes()
     var segundo = data.getSeconds()
+    const month = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
+    var dia = data.getDate()
+    var mes = month[data.getMonth()];
+    var ano = data.getFullYear()
     msg.innerHTML = `Agora são ${hora} hora(s) ${minuto} minuto(s) e ${segundo} segundo(s). ${fusoHorario}`
+    msg3.innerHTML = `${dia} / ${mes} / ${ano}`
     setTimeout(function() {carregar()}, 250);
 
     if (hora >= 0 & hora < 12){
