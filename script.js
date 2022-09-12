a2d77763526fa3da5b9fee44df492731e708a64e
Matheus <matheusdearaujo1209@gmail.com>
2022-09-12 19:18:49 -0300

    Horário do Dia

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
     function carregar(){
     var msg = window.document.getElementById('texto')
     var msg2 = window.document.getElementById('texto2')
-    var img = window.document.getElementById('imagem')
     var data = new Date()
     var fusoHorario = Intl.DateTimeFormat().resolvedOptions().timeZone;
     var hora = data.getHours()
     var minuto = data.getMinutes()
     var segundo = data.getSeconds()
     msg.innerHTML = `Agora são ${hora} hora(s) ${minuto} minuto(s) e ${segundo} segundo(s). ${fusoHorario}`
-    setTimeout(function() {carregar()}, 500);
+    setTimeout(function() {carregar()}, 250);
 
     if (hora >= 0 & hora < 12){
         msg2.innerHTML = "Bom Dia!"
-    img.innerHTML = '<img src="imgs/foto-manha.png">'
     document.body.style.backgroundImage = "url('imgs/bg-manha.jpg')"
     }
     else if (hora >= 12 & hora < 18) {
         msg2.innerHTML = "Boa Tarde!"
-    img.innerHTML = '<img src="imgs/foto-tarde.png">'
     document.body.style.backgroundImage = "url('imgs/bg-tarde.jpg')"
     }
     else {
         msg2.innerHTML = "Boa Noite!"
-    img.innerHTML = '<img src="imgs/foto-noite.png">'
     document.body.style.backgroundImage = "url('imgs/bg-noite.jpg')"
     }
 }
